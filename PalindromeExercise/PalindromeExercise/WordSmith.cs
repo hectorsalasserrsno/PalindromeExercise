65e4e4b184c3369e964590be32d12f0acd6a34ba
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-16 22:19:18 -0500

    exercise done

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str) 
+        {
+            string reversedWord = string.Empty;
+
+            for (int i = str.Length -1; i >= 0; i--) 
+                {
+                reversedWord += str[i];
+                }
+            if (str == reversedWord) 
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("salas", true)]
+        [InlineData("hannah", true)]
+        public void Test1(string word, bool expected )
         {
+            //Arrange- prepare code
+            var  instance = new WordSmith();
 
+            //Act- call method
+            var actual = instance.IsAPalindrome(word);
+
+
+
+            //Assert- compare
+            Assert.Equal(expected, actual);
         }
     }
 }
